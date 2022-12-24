5f666db39a1d582144dfc3a1da6e5bdc4db30387
Bogdan <67187037+bogdandrienko@users.noreply.github.com>
2022-12-24 12:17:23 +0600

    Create clear.py

---
+++ 5_extra/clear_all_github_projects/clear.py
@@ -0,0 +1,16 @@
+import os
+import shutil
+
+if __name__ == "__main__":
+    path = r'C:\github_projects'
+    for root, dirs, files in os.walk(path, topdown=False):
+        # for name in files:
+        #     os.remove(os.path.join(root, name))
+        for folder in dirs:
+            print(folder)
+            if folder in [".idea", "env", "venv", "node_modules"]:
+                try:
+                    # os.rmdir(os.path.join(root, folder))
+                    shutil.rmtree(os.path.join(root, folder))
+                except Exception as error:
+                    print(error)
